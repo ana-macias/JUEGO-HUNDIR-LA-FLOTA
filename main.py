206361109b49a79299c35e9bbf1a02acd36c77b0
ana-macias <amaciaselopez@gmail.com>
2025-05-08 16:24:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import utils
+#import variables as vr
+
+
+
+print("TABLERO DONDE SE MUESTRAN LOS BARCOS DEL RIVAL Y LOS DISPAROS DEL JUGADOR.")
+print(utils.tablero_rival_barcos)
+print(utils.colocar_barcos_manual(utils.tablero_jugador_barcos))
+
+print("TABLERO DONDE SE MUESTRAN LOS DISPAROS DEL RIVAL")
+print(utils.tablero_rival_disparos)
+print(utils.colocar_barcos_aleatorio(utils.tablero_jugador_barcos))
+
+print("___________________________________")
+
+
+
+
+
+
